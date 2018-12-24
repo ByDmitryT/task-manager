246ed840268a57281aa7526e0ad4cbe90a3d7899
Титов Дмитрий <de.titov@msk.i-teco.ru>
2018-12-24 16:48:59 +0300

    init

---
+++ src/test/java/iteco/study/AppTest.java
@@ -0,0 +1,14 @@
+package iteco.study;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
