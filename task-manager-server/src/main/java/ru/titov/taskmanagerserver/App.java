105803260dfb66b45bf63f29784fc78a2e8b797d
Dmitry Titov <dtitov94@gmail.com>
2019-02-07 13:42:25 +0300

    Revert "added xml spring configuration transactional"
    This reverts commit 032d67dd093b687a7764562985ce6671d2e3317c.

--- task-manager-server/src/main/java/ru/titov/taskmanagerserver/App.java
+++ task-manager-server/src/main/java/ru/titov/taskmanagerserver/App.java
@@ -1,13 +1,14 @@
 package ru.titov.taskmanagerserver;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.titov.taskmanagerserver.api.controller.Bootstrap;
+import ru.titov.taskmanagerserver.config.AppConfig;
 
 public class App {
 
     public static void main(String[] args) {
-        final ApplicationContext context = new ClassPathXmlApplicationContext("application-config.xml");
+        final ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         final Bootstrap bootstrap = context.getBean(Bootstrap.class);
         bootstrap.run();
     }
